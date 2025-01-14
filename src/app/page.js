837b0e3552a9e37b80f2859fa875476bd9486802
Saad Baig <saadbaig7777@gmail.com>
2025-01-14 11:38:19 +0500

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,5 @@
-import Link from 'next/link'
-import styles from './page.module.scss'
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1>Agriculture Management System</h1>
-      
-      <nav className={styles.navigation}>
-        <Link href="/clients" className={styles.navLink}>
-          Manage Clients
-        </Link>
-        <Link href="/workers" className={styles.navLink}>
-          Manage Workers
-        </Link>
-        <Link href="/fields" className={styles.navLink}>
-          Manage Fields
-        </Link>
-        <Link href="/harvests" className={styles.navLink}>
-          Manage Harvests
-        </Link>
-      </nav>
-    </main>
-  )
+  redirect("/clients");
 }
